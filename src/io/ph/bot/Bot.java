1cec780620baf7bdfff422cf5119211d7961514c
Paul <22379523+paul-io@users.noreply.github.com>
2018-01-28 20:13:26 -0500

    2.1.0

--- src/io/ph/bot/Bot.java
+++ src/io/ph/bot/Bot.java
@@ -53,7 +53,7 @@ public class Bot {
 
 	// Set to true if you want various debug statements
 	public static final boolean DEBUG = false;
-	public static final String BOT_VERSION = "v2.1.0beta";
+	public static final String BOT_VERSION = "v2.1.0";
 	public static boolean isReady = false;
 
 	private APIKeys apiKeys = new APIKeys();
