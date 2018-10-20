70d042c38a23014312a4c4d7016e23eb2b0e961b
Ken Swenson <flat@esoteric.moe>
2018-10-20 19:56:31 -0400

    Update Info command

--- src/io/ph/bot/commands/general/Info.java
+++ src/io/ph/bot/commands/general/Info.java
@@ -31,16 +31,12 @@ public class Info extends Command {
 		EmbedBuilder em = new EmbedBuilder();
 		em.setTitle("Hi, I'm " + msg.getGuild().getMember(msg.getJDA().getSelfUser()).getEffectiveName(), null)
 		.setColor(Util.resolveColor(Util.memberFromMessage(msg), Color.MAGENTA))
-		.addField("Repository", "<https://momobot.io/github>", true)
-		.addField("Help server", "<https://momobot.io/join>", true)
+		.addField("Repository", "<https://github.com/Flat/Momo-2>", true)
 		.addField("Invite link", Bot.getInstance().getConfig().getBotInviteLink(), true)
-		.addField("Command list", "<https://momobot.io/commands.html>", true)
-		.setDescription("I can do a lot of things! Too many to list here, though. Feel free to take a look "
-				+ "through the links below, though, to get a quick rundown of my features")
 		.setThumbnail(msg.getJDA().getSelfUser().getAvatarUrl())
-		.setFooter(String.format("Version %s | Made with <3 by %s", 
-				Bot.BOT_VERSION,
-				"Kagumi"), null);
+		.setFooter(String.format("Version %s", 
+				Bot.BOT_VERSION
+				), null);
 		msg.getChannel().sendMessage(em.build()).queue();
 	}
 
